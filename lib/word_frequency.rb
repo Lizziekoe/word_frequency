bc7484e71392990e58330213e59169844ba8c3d3
Lizzie <lizziefkoehler@gmail.com>
2015-01-09 11:43:55 -0800

    refactor method to account for upper and lowercase

--- lib/word_frequency.rb
+++ lib/word_frequency.rb
@@ -1,10 +1,10 @@
 class String
-	define_method(:word_frequency) do |match_word|
-
-		if self.include?(match_word)
-			string_to_array = self.split() 
-			string_to_array.count(match_word) 
-
-		end
+	define_method(:word_frequency) do |match_word|	
+		self.downcase!()	
+		match_word.downcase!()
+			if self.include?(match_word)
+				string_to_array = self.split() 
+				string_to_array.count(match_word)
+			end
 	end
 end
--- spec/word_frequency_spec.rb
+++ spec/word_frequency_spec.rb
@@ -9,4 +9,12 @@ describe ('String#word_frequency') do
 	it('takes a string, and then a matching singular word, and returns the word frequency value from the original string') do
 		expect(("I went to the store store store store").word_frequency("store")).to(eq(4))
 	end
+
+	it('takes a string with both upcase and lowercase letters and still returns the frequency if the words match') do
+		expect(("I went to the STORE store STORE store").word_frequency("store")).to(eq(4))
+	end
+
+	it('takes a string with lowercase and a chosen word with uppercase and still returns the frequency if the words match') do
+		expect(("I went to the store store store store").word_frequency("STORE")).to(eq(4))
+	end
 end
